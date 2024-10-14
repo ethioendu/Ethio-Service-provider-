2df3f366734932db453bc07a2a1093d52fc5577a
Endalsh Ayalew <ethioendu21@yahoo.com>
2024-10-14 20:46:51 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,374 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="This website give easy way to find any Service in Ethiopia">
+  <meta name="keyword" content="ethiopia, service, find, where ">
+  <meta name="author" content="Endalsh Ayalew">
+
+
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+  <title>Ethio Service Provider </title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="assets/css/fontawesome.css">
+  <link rel="stylesheet" href="assets/css/Ethio-Service-Provider.css">
+  <link rel="stylesheet" href="assets/css/animated.css">
+  <link rel="stylesheet" href="assets/css/owl.css">
+
+</head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** -->
+            <a href="index3.html" class="logo">
+            </a>
+            <!-- ***** Logo End ***** -->
+            <!-- ***** Menu Start ***** -->
+            <ul class="nav">
+              <li><a href="index3.html" class="active">Home</a></li>
+              <li><a href="category1.html">Category</a></li>
+              <li><a href="listing.html">Listing</a></li>
+              <li><a href="contact.html">Contact Us</a></li>
+              <li>
+                <div class="main-white-button"><a href="listing.html"><i class="fa fa-plus"></i> Add Your Listing</a></div>
+              </li>
+            </ul>
+            <a class='menu-trigger'>
+              <span>Menu</span>
+            </a>
+            <!-- ***** Menu End ***** -->
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <div class="main-banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="top-text header-text">
+            <h6>Over 36,500+ Active Listings</h6>
+            <h2>Find Nearby Places &amp; Things</h2>
+          </div>
+        </div>
+        <div class="col-lg-12">
+          <form id="search-form" name="gs" method="submit" role="search" action="#">
+            <div class="row">
+              <div class="col-lg-3 align-self-center">
+                <fieldset>
+                  <select name="area" class="form-select" aria-label="Area" id="chooseCategory" onchange="this.form.click()">
+                    <option selected>All Areas</option>
+                    <option value="New Village">New Village</option>
+                    <option value="Old Town">Old Town</option>
+                    <option value="Modern City">Modern City</option>
+                  </select>
+                </fieldset>
+              </div>
+              <div class="col-lg-3 align-self-center">
+                <fieldset>
+                  <input type="address" name="address" class="searchText" placeholder="Enter a location" autocomplete="on" required>
+                </fieldset>
+              </div>
+              <div class="col-lg-3 align-self-center">
+                <fieldset>
+                  <select name="price" class="form-select" aria-label="Default select example" id="chooseCategory" onchange="this.form.click()">
+                    <option selected>Price Range</option>
+                    <option value="$100 - $250">$100 - $250</option>
+                    <option value="$250 - $500">$250 - $500</option>
+                    <option value="$500 - $1000">$500 - $1,000</option>
+                    <option value="$1000+">$1,000 or more</option>
+                  </select>
+                </fieldset>
+              </div>
+              <div class="col-lg-3">
+                <fieldset>
+                  <button class="main-button"><i class="fa fa-search"></i> Search Now</button>
+                </fieldset>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="col-lg-10 offset-lg-1">
+          <ul class="categories">
+            <li><a href="listing.html"><span class="icon"><img src="assets/images/search-icon-01.png" alt="Home"></span> Real Estate</a></li>
+            <li><a href="listing.html"><span class="icon"><img src="assets/images/search-icon-02.png" alt="Food"></span> Food &amp; Entertainment </a></li>
+            <li><a href="listing.html"><span class="icon"><img src="assets/images/search-icon-03.png" alt="Vehicle"></span> Cars</a></li>
+            <li><a href="listing.html"><span class="icon"><img src="assets/images/search-icon-04.png" alt="Shopping"></span> Shopping</a></li>
+            <li><a href="listing.html"><span class="icon"><img src="assets/images/search-icon-05.png" alt="Travel"></span> Traveling</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="popular-categories">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="section-heading">
+            <h2>Find Government & Organization Service</h2>
+            <h6>make appointment </h6>
+          </div>
+
+
+
+        </div>
+        <div class="col-lg-12">
+          <div class="naccs">
+            <div class="grid">
+              <div class="row">
+                <div class="col-lg-3">
+                  <div class="menu">
+                    <div class="first-thumb active">
+                      <div class="thumb">
+                        <span class="icon"><img src="assets/images/health1.jpg" alt=""></span>
+                        Health
+                      </div>
+                    </div>
+                    <div>
+                      <div class="thumb">
+                        <span class="icon"><img src="assets/images/teleicon.jpg" alt=""></span>
+                        Telecommunication
+                      </div>
+                    </div>
+                    <div>
+                      <div class="thumb">
+                        <span class="icon"><img src="assets/images/utility-icon.jpg" alt=""></span>
+                        Utility Service
+                      </div>
+                    </div>
+                    <div>
+                      <div class="thumb">
+                        <span class="icon"><img src="assets/images/transport-icon.png" alt=""></span>
+                        Transportation
+                      </div>
+                    </div>
+                    <div class="last-thumb">
+                      <div class="thumb">
+                        <span class="icon"><img src="assets/images/secur-icon.jpg" alt=""></span>
+                        Security Office
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-9 align-self-center">
+                  <ul class="nacc">
+                    <li class="active">
+                      <div>
+                        <div class="thumb">
+                          <div class="row">
+                            <div class="col-lg-5 align-self-center">
+                              <div class="left-text">
+                                <h4>Ethiopia Health facilities</h4>
+                                <p>here you can find most health facilities in Ethiopia.
+                                  you can check the opening and the closing hours of the facilities you can also book appointment if the facilities you are looking for take online appointment.</p>
+                                <div class="main-white-button"><a href="category1.html"><i class="fa fa-eye"></i> Discover More</a></div>
+                              </div>
+                            </div>
+                            <div class="col-lg-7 align-self-center">
+                              <div class="right-image">
+                                <img src="assets/images/health.jpg" alt="Health">
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <div class="thumb">
+                          <div class="row">
+                            <div class="col-lg-5 align-self-center">
+                              <div class="left-text">
+                                <h4>Ethiopia Telecommunications facilities</h4>
+                                <p>here you can find most Telecommunication facilities in Ethiopia. you can check the opening and the closing hours of the facilities you can also book appointment if the facilities you are looking for take online appointment.</p>
+                                <div class="main-white-button"><a href="category1.html"><i class="fa fa-eye"></i> Explore More</a></div>
+                              </div>
+                            </div>
+                            <div class="col-lg-7 align-self-center">
+                              <div class="right-image">
+                                <img src="assets/images/tele.jpg" alt="Telecommunication">
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <div class="thumb">
+                          <div class="row">
+                            <div class="col-lg-5 align-self-center">
+                              <div class="left-text">
+                                <h4>Ethiopia Utility facilities</h4>
+                                <p>here you can find most Utility facilities in Ethiopia. you can check the opening and the closing hours of the Utility facilities you can also book appointment if the facilities you are looking for take online appointment.</p>
+                                <div class="main-white-button"><a href="category1.html"><i class="fa fa-eye"></i> More Listing</a></div>
+                              </div>
+                            </div>
+                            <div class="col-lg-7 align-self-center">
+                              <div class="right-image">
+                                <img src="assets/images/utility.jpg" alt="cars in the city">
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <div class="thumb">
+                          <div class="row">
+                            <div class="col-lg-5 align-self-center">
+                              <div class="left-text">
+                                <h4>Ethiopia Transportation facilities</h4>
+                                <p>here you can find most Transportation facilities in Ethiopia. you can check the opening and the closing hours of the Transportation facilities you can also book appointment if the facilities you are looking for take online appointment.</p>
+                                <div class="main-white-button"><a href="category1.html"><i class="fa fa-eye"></i> Discover More</a></div>
+                              </div>
+                            </div>
+                            <div class="col-lg-7 align-self-center">
+                              <div class="right-image">
+                                <img src="assets/images/transportaion.jpg" alt="transportaion">
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <div class="thumb">
+                          <div class="row">
+                            <div class="col-lg-5 align-self-center">
+                              <div class="left-text">
+                                <h4>Security office & Information and Safety Tips </h4>
+                                <p>here you can find most Security office in Ethiopia. you can check the opening and the closing hours of the Security office you can also book appointment if the facilities you are looking for take online appointment.</p>.</p>
+                                <div class="main-white-button"><a rel="nofollow" href="category1.html"><i class="fa fa-eye"></i> Read More</a></div>
+                              </div>
+                            </div>
+                            <div class="col-lg-7 align-self-center">
+                              <div class="right-image">
+                                <img src="assets/images/security.jpg" alt="security">
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="contact-page">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="inner-content">
+            <div class="row">
+              <div class="col-lg-6">
+                <div id="map">
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d142700.56658583536!2d38.7781448!3d8.96317685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85cef5ab402d%3A0x8467b6b037a24d49!2sAddis%20Ababa!5e1!3m2!1sen!2set!4v1728128484609!5m2!1sen!2set" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+
+                </div>
+              </div>
+
+              <div class="col-lg-6 align-self-center">
+                <form id="contact" action="" method="get">
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <fieldset>
+                        <input type="name" name="name" id="name" placeholder="Name" autocomplete="on" required>
+                      </fieldset>
+                    </div>
+                    <div class="col-lg-6">
+                      <fieldset>
+                        <input type="surname" name="surname" id="surname" placeholder="Surname" autocomplete="on" required>
+                      </fieldset>
+                    </div>
+                    <div class="col-lg-12">
+                      <fieldset>
+                        <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your Email" required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-lg-12">
+                      <ul>
+                        <li><input type="checkbox" name="option1" value="cars"><span>Cars</span></li>
+                        <li><input type="checkbox" name="option2" value="aparmtents"><span>Apartments</span></li>
+                        <li><input type="checkbox" name="option3" value="shopping"><span>Shopping</span></li>
+                        <li><input type="checkbox" name="option4" value="food"><span>Food &amp; Entertainment </span></li>
+                        <li><input type="checkbox" name="option5" value="traveling"><span>Traveling</span></li>
+                      </ul>
+                    </div>
+                    <div class="col-lg-12">
+                      <fieldset>
+                        <textarea name="message" type="text" class="form-control" id="message" placeholder="Message" required=""></textarea>
+                      </fieldset>
+                    </div>
+                    <div class="col-lg-12">
+                      <fieldset>
+                        <button type="submit" id="form-submit" class="main-button "><i class="fa fa-paper-plane"></i> Send Message</button>
+                      </fieldset>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <?php include "include/footer.php" ?>
+
+
+  <!-- Scripts -->
+
+
+
+
+</body>
+
+</html>
